9d9e0ab096f50870417d6a7cf77cfa8e8b9913f3
Tim Davis <contact@timdav.is>
2016-09-11 15:29:22 -0700

    Progress on #5 #7.

--- src/matches.test.js
+++ src/matches.test.js
@@ -2,11 +2,11 @@ import { describe, it } from 'global'
 import { expect } from 'chai'
 import matches from './matches'
 
-describe('matches(Type, obj)', () => {
+describe('matches(Type, value)', () => {
 
   it('knows whether an Array populated with Objects matches a Type', () => {
     const Type = [{ name: String, birthday: Date }]
-    const obj = [
+    const arr = [
       {
         name: 'Dennis Ritchie',
         birthday: new Date('September 9, 1941')
@@ -20,25 +20,20 @@ describe('matches(Type, obj)', () => {
         birthday: new Date('December 1960')
       }
     ]
-    expect(matches(Type, obj)).to.be.true
+    expect(matches(Type, arr)).to.be.true
   })
 
-  it('knows whether a complex object matches a Type', () => {
-    const Pet = { name: String, weight: Number }
-    const Owner = { name: String, pets: [Pet] }
-    const garfield = {
-      name: 'Garfield',
-      weight: 40
-    }
-    const odie = {
-      name: 'Odie',
-      weight: 17
-    }
-    const john = {
+  it('knows whether a complex Object matches a Type', () => {
+    const Owner = { name: String, pets: [{ name: String, weight: Number }] }
+    const owner = {
       name: 'John Arbuckle',
-      pets: [garfield, odie, 1]
+      pets: [
+        { name: 'Garfield', weight: 40 },
+        { name: 'Odie', weight: 17 },
+        1
+      ]
     }
-    expect(matches(Owner, john)).to.be.false
+    expect(matches(Owner, owner)).to.be.false
   })
 
 })
